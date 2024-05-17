a204d0303ff520515aaf14a5989948d528884576
kenza <kenza.boumraime@gmail.com>
2024-05-17 23:29:57 +0100

    Adding front for Editing blogs

--- src/components/EditBlogForm.jsx
+++ src/components/EditBlogForm.jsx
@@ -2,10 +2,15 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import Input from "./UI/Input";
+import TextArea from "./UI/TextArea";
+import Button from "./UI/Button"
+import Image from "next/image";
 
 export default function EditTopicForm({ id, title, content }) {
   const [newTitle, setNewTitle] = useState(title);
   const [newContent, setNewContent] = useState(content);
+  const [error, setError] = useState(null); // State to hold error
 
   const router = useRouter();
 
@@ -28,31 +33,29 @@ export default function EditTopicForm({ id, title, content }) {
       router.refresh();
       router.push("/");
     } catch (error) {
-      console.log(error);
+      setError(error.message); // Set error state with error message
     }
   };
-
+ 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-      <input
-        onChange={(e) => setNewTitle(e.target.value)}
-        value={newTitle}
-        className="border border-slate-500 px-8 py-2"
-        type="text"
-        placeholder="Blog Title"
-      />
+    <div className="tw-flex tw-items-center">
+    <div className="tw-w-1/3">
+    <Image style={{ borderRadius: '7px' }} src="/tt.webp" alt="" class="tw-w-full"  width={300} height={100}/>
+    </div>
+    <div className="tw-w-2/3"> 
+    <form className="tw-max-w-sm tw-mx-auto" onSubmit={handleSubmit}>
+     <Input name="Blog Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+     <TextArea name="Blog Content" value={content} onChange={(e) => setContent(e.target.value)} />
+     <Button name="Add Blog"/>
+     {error && <p className="error-message">{error}</p>}
+  </form>
+  </div>
+  
+  </div>
+    
+    
 
-      <input
-        onChange={(e) => setNewContent(e.target.value)}
-        value={newContent}
-        className="border border-slate-500 px-8 py-2"
-        type="text"
-        placeholder="Blog Content"
-      />
 
-      <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
-        Update Blog
-      </button>
-    </form>
+
   );
 }
